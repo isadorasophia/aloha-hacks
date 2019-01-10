a9437f220b48526225ea65cc1ede35e45918cc32
Laurent Lousky <t-lalous@microsoft.com>
2019-01-09 16:38:09 -0800

    Script to destroy ball after certain distance

---
+++ Assets/scripts/DestroyScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour
+
+{
+	[SerializeField] private double DistanceThreshold;
+	[SerializeField] private string DistanceFromTag;
+    private GameObject MarkerObject;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        MarkerObject = GameObject.FindGameObjectWithTag(DistanceFromTag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		if (Vector3.Distance(MarkerObject.transform.position, transform.position) >= DistanceThreshold)
+			Destroy(gameObject);
+    }
+}
