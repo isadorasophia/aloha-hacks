65e17e7a208640611068301db9d4944dfa1a0803
Sagar Manchanda <sagmanch@microsoft.com>
2019-01-10 12:25:02 -0800

    Scoring finally works

--- Assets/scripts/CrateScriptInner.cs
+++ Assets/scripts/CrateScriptInner.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CrateScriptInner : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private int score;
+    public Text scoreText;
+
     void Start()
     {
-        
+        score = 0;
+        ScoreUpdate();
     }
 
     // Update is called once per frame
@@ -16,8 +22,18 @@ public class CrateScriptInner : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter( Collision collision )
+    private void OnTriggerEnter( Collider other )
     {
-        int DebugThis = 0;
+       if (other.gameObject.CompareTag("Ball"))
+        {
+            Debug.Log("WE GOT A HIT");
+            score += 1;
+            ScoreUpdate();
+        }
+    }
+
+    void ScoreUpdate()
+    {
+        scoreText.text = "Score: " + score.ToString();
     }
 }
