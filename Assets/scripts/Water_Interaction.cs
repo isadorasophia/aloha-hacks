689648e10219795a4d4008ff3d4f9807ab8cfa42
Adrian Pang <t-adpang@microsoft.com>
2019-01-09 16:41:28 -0800

    environmental interaction and splash effect

---
+++ Assets/scripts/Water_Interaction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water_Interaction : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _particleLayers;
+    public GameObject _WaterParticlePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collider col)
+    {
+        if (CheckForParticleLayer(col.gameObject.layer))
+        {
+            Vector3 pNewParticlePos = col.gameObject.transform.position;
+            GameObject pNewParticle = Instantiate(_WaterParticlePrefab);
+            pNewParticle.transform.position = pNewParticlePos;
+            Destroy(pNewParticle, 2f);
+        }
+    }
+
+    private bool CheckForParticleLayer(LayerMask layer)
+    {
+        return layer == (_particleLayers | (1 << layer));
+    }
+}
