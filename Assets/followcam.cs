aae26fb245fbc64ab23ebd4941bb1ea16719991f
reveza <rebeccavezinacote@gmail.com>
2019-01-10 10:08:10 -0800

    Add basic text

---
+++ Assets/followcam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followcam : MonoBehaviour
+{
+    GameObject mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mainCam.transform.position + 3 *  mainCam.transform.forward;
+    }
+}
